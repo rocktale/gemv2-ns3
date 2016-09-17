f62e93824e8e148b688dc049ba6143192c4de5f6
Karsten <rocktale@users.noreply.github.com>
2016-09-17 18:54:12 +0200

    Added basic ns-3 model structure
    The code was generated using 'create-module.py' from ns-3 3.25.

---
+++ gemv2/helper/gemv2-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef GEMV2_HELPER_H
+#define GEMV2_HELPER_H
+
+#include "ns3/gemv2.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* GEMV2_HELPER_H */
+
---
+++ gemv2/model/gemv2.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef GEMV2_H
+#define GEMV2_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* GEMV2_H */
+
